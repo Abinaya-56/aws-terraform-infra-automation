09c8d0248902d3988444357b0c887c6528be39ce
Abinaya <abinayasp0211@gmail.com>
2026-01-20 13:33:12 +0000

    Two public subnets created

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,24 @@ resource "aws_vpc" "main" {
     Name = "terraform-project-vpc"
   }
 }
+
+resource "aws_subnet" "public_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
