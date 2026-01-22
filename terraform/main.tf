8e3366ed40e7ab0d663060b971fca5e585e737d9
Abinaya <abinayasp0211@gmail.com>
2026-01-22 16:06:43 +0000

    EC2 instance created via Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -105,3 +105,43 @@ resource "aws_vpc_security_group_egress_rule" "all_outbound" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
+
+resource "aws_key_pair" "terraform_key" {
+  key_name   = "terraform-key"
+  public_key = file("~/.ssh/terraform-key.pub")
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent      = true
+  owners           = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  subnet_id              = aws_subnet.public_1.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = aws_key_pair.terraform_key.key_name
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "terraform-web-server"
+  }
+}
