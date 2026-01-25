455f6c3edae354a3c6fb994c465e99bb961b1d9a
Abinaya <abinayasp0211@gmail.com>
2026-01-25 10:32:21 +0000

    Phase 9: Terraform outputs added and captured

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,12 @@
+
 output "ec2_public_ip" {
   value = aws_instance.web_server.public_ip
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.project_bucket.bucket
 }
